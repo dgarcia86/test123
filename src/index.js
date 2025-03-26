a3551eaf4da139f8ec446fb37a1006f94160b5c4
dgarcia86 <34232429+dgarcia86@users.noreply.github.com>
2025-03-26 10:55:38 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { render } from 'react-snapshot';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
