2be4a030cf55c31d7da9c2a4cc9a9630585b3723
dgarcia86 <34232429+dgarcia86@users.noreply.github.com>
2025-03-26 10:52:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>¡Hola Mundo!</h1>
+      <p>Este es un ejemplo pre-renderizado de una aplicación React.</p>
+    </div>
+  );
+}
+
+export default App;
