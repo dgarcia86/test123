a17392f4623ccc43ccef5c100e95c70f0d8a69da
dgarcia86 <34232429+dgarcia86@users.noreply.github.com>
2025-03-26 10:55:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>¡Hola Mundo!</h1>
-      <p>Este es un ejemplo de una aplicación React pre-renderizada.</p>
+      <p>Este es un ejemplo básico de una aplicación React.</p>
     </div>
   );
 }
